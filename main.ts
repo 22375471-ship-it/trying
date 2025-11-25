24f33c27d8813ba5394209a5a18bbac6a421297e
22375471-ship-it <22375471@student.ncad.ie>
2025-11-25 21:03:08 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let Image2: Image = null
+kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(true))
+kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Normal)
+basic.showString("Hello!")
+Image2.showImage(0)
+basic.forever(function () {
+	
+})
